ab876b316f2c39edd6a20c08859bed16de4215d6
sande-swalah <alvinswalah@gmail.com>
2025-11-24 21:49:03 +0300

    final push

--- index.js
+++ index.js
@@ -31,32 +31,41 @@ const day28TempC =17
 const day29TempF =76
 const day30TempC =29
 
-//* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
+//Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
 
+// Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
+function fToC(F){ 
+     return (F - 32) * 5 / 9
+}
 // to convert temperature from fahrenheit to degrees Celsiusl
-const day1TempC = (day1TempF - 32) * 5 / 9
-const day3TempC = (day3TempF - 32) * 5 / 9
-const day5TempC = (day5TempF - 32) * 5 / 9
-const day7TempC = (day7TempF - 32) * 5 / 9
-const day9TempC = (day9TempF - 32) * 5 / 9
-const day11TempC = (day11TempF - 32) * 5 / 9
-const day13TempC = (day13TempF - 32) * 5 / 9
-const day15TempC = (day15TempF - 32) * 5 / 9
-const day17TempC = (day17TempF - 32) * 5 / 9
-const day19TempC = (day19TempF - 32) * 5 / 9
-const day21TempC = (day21TempF - 32) * 5 / 9
-const day23TempC = (day23TempF - 32) * 5 / 9
-const day25TempC = (day25TempF - 32) * 5 / 9
-const day27TempC = (day27TempF - 32) * 5 / 9
-const day29TempC = (day29TempF - 32) * 5 / 9
+const day1TempC = fToC(day1TempF)
+const day3TempC = fToC(day3TempF)
+const day5TempC = fToC(day5TempF)
+const day7TempC = fToC(day7TempF)
+const day9TempC = fToC(day9TempF)
+const day11TempC = fToC(day11TempF)
+const day13TempC = fToC(day13TempF)
+const day15TempC = fToC(day15TempF)
+const day17TempC = fToC(day17TempF)
+const day19TempC = fToC(day19TempF)
+const day21TempC = fToC(day21TempF)
+const day23TempC = fToC(day23TempF)
+const day25TempC = fToC(day25TempF)
+const day27TempC = fToC(day27TempF)
+const day29TempC = fToC(day29TempF)
+
+
 
 
 //! Start the calculation of the total temperatures
-const tot_temperature_in_celsius = day1TempC + day2TempC + day3TempC + day4TempC + day5TempC + day6TempC + day7TempC + day8TempC + day9TempC + day10TempC + day11TempC + day12TempC + day13TempC + day14TempC + day15TempC + day16TempC + day17TempC + day18TempC + day19TempC + day20TempC + day21TempC + day22TempC + day23TempC + day24TempC + day25TempC + day26TempC + day27TempC + day28TempC + day29TempC + day30TempC 
+const tot_temperature_in_celsius = (day1TempC + day2TempC + day3TempC + day4TempC + day5TempC + day6TempC + day7TempC + day8TempC + day9TempC + day10TempC + day11TempC + day12TempC + day13TempC + day14TempC + day15TempC + day16TempC + day17TempC + day18TempC + day19TempC + day20TempC + day21TempC + day22TempC + day23TempC + day24TempC + day25TempC + day26TempC + day27TempC + day28TempC + day29TempC + day30TempC)
 
 //* Then apply the conversion to calculate the total in the other unit of measurement
 
-const tot_temperature_in_fahrenheit = (tot_temperature_in_celsius * 9/5)+32
+function CtoF (C){ 
+     return (C * 9/5) + 32
+}
+const tot_temperature_in_fahrenheit = CtoF (tot_temperature_in_celsius)
 
 //* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
 
